d84ffad3d2de39a196127a0507264302caa972d5
buyaramadhan <ytchochote@gmail.com>
2023-06-21 15:21:26 +0700

    commit6

--- ioFileWithPath/ioFileWithPath.cpp
+++ ioFileWithPath/ioFileWithPath.cpp
@@ -1,20 +1,42 @@
-// ioFileWithPath.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include <fstream>
 #include <iostream>
+#include <string>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+	string baris;
+	string NamaFile;
+
+	cout << "Masukan Nama File : ";
+	cin >> NamaFile;
+
+	//membuka file dalam mode menulis
+	ofstream outfile;
+	//menunjuk ke sebuah nama file 
+	outfile.open(NamaFile + ".txt", ios::out);
+
+	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+	while (true) {
+		cout << "_ ";
+		getline(cin, baris);
+		if (baris == "q") break;
+		outfile << baris << endl;
+	}
+	outfile.close();
+
+	ifstream infile;
+	infile.open(NamaFile + ".txt");
+
+	cout << endl << ">= Membuka dan membaca file " << endl;
+	if (infile.is_open())
+	{
+		while (getline(infile, baris))
+		{
+			cout << baris << '\n';
+		}
+		infile.close();
+	}
+	else cout << "Unable to open file";
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
