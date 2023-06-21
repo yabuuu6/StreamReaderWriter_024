38836aca4f2d0db746d594b0da2f09b78276a725
buyaramadhan <ytchochote@gmail.com>
2023-06-21 15:51:38 +0700

    commit15

--- exceptionStandard/exceptionStandard.cpp
+++ exceptionStandard/exceptionStandard.cpp
@@ -18,4 +18,7 @@ int main()
 		cout << e.what() << endl;
 		/* akan dieksekusi karna array data hanya memiliki 3 elemen*/
 	}
+	cout << "Baris Program Yang Terakhir " << endl;
+	/* penanda 2: bahwa program berjalan tanpa berhenti meskipun terjadi kesalahan */
+	return 0;
 }
