286615fde5027ecba42f0a4e31486f772635a446
buyaramadhan <ytchochote@gmail.com>
2023-06-21 14:43:02 +0700

    Add project files.

---
+++ ioFile/ioFile.cpp
@@ -0,0 +1,15 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string baris;
+	
+	// membuka file dalam mode menulis.
+	ofstream outfile;
+	// menunjuk ke sebuah nama file 
+	outfile.open("contohfile.txt");
+
+	cout <<
+}
