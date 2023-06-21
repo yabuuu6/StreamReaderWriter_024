38aa033bad8a775fa833aae4c392b7e2a2c33fac
buyaramadhan <ytchochote@gmail.com>
2023-06-21 15:03:30 +0700

    commit5

--- ioFile/ioFile.cpp
+++ ioFile/ioFile.cpp
@@ -41,6 +41,10 @@ int main() {
 			//dan tampilkan di sini
 			cout << baris << '\n';
 		}
-		//tutup
+		//tutup file tersebut setelah selesai
+		infile.close();
 	}
+	//jika tidak ditemukan file maka akan menampilkan ini
+	else cout << "Unable to open file";
+	return 0;
 }
