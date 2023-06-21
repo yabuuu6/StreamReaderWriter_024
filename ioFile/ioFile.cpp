aa151ac2a0ede7488d74b1896e50c2a3e7361fa4
buyaramadhan <ytchochote@gmail.com>
2023-06-21 15:01:33 +0700

    commit4

--- ioFile/ioFile.cpp
+++ ioFile/ioFile.cpp
@@ -23,4 +23,24 @@ int main() {
 		//menulis dan memasukan nilai dari 'baris' ke dalam file 
 		outfile << baris << endl;
 	}
+//selesai dalam menulis sekarang tutup filenya
+	outfile.close();
+
+	//Membuka file dalam mode membaca 
+	ifstream infile;
+	//menunjuk ke sebuah file
+	infile.open("contohfile.txt");
+
+	cout << endl << ">= Membuka dan membaca file " << endl;
+	//jika file ada maka 
+	if (infile.is_open())
+	{
+		//melakukan perulangan setiap baris 
+		while (getline(infile, baris))
+		{
+			//dan tampilkan di sini
+			cout << baris << '\n';
+		}
+		//tutup
+	}
 }
